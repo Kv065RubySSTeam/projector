66454417723866993b4c4ac563cb466000bd1526
psidae <akson.rill@gmail.com>
2020-04-27 23:07:55 +0300

    Update models and controller

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,11 +1,24 @@
 class MembershipsController < ApplicationController
   before_action :current_board
 
-  def create; end
+  def create
+    @membership = Membership.new(params[:membership])
+    unless @membership&.nil?
+      @membership.save!
+    end
+  end
 
-  def update; end
+  def update
+    @user = Membership.find_by_id(params[:user_id])
+    unless @user.nil? && current_user.board_admin
+      @user.admin = true
+      @user.save!
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @membership.delete! unless @membership.nil? && curren_user.board_admin
+  end
 
   private
 
