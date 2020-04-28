783653914b73b913809aa127f5d55155f02bcf6e
psidae <akson.rill@gmail.com>
2020-04-28 11:05:38 +0300

    Update controller

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,28 +1,36 @@
 class MembershipsController < ApplicationController
-  before_action :current_board
+  before_action :load_board
+  before_action :check_user_permission
 
   def create
-    @membership = Membership.new(params[:membership])
-    if @membership&.nil? && current_user.board_admin
-      @membership.save!
+    @user = User.find(params[:user_id])
+    if @board.memberships.create(user_id: user.id, admin: false) 
+      flash[:success] = 'New user is added!'
+    else 
+      flash[:danger] = 'New user was not added!'
+      redirect_back fallback_location: root_path
     end
   end
 
   def update
-    @user = Membership.find_by_id(params[:user_id])
-    if !@user.nil? && current_user.board_admin
-      @user.admin = true
-      @user.save!
-    end
-  end
-
-  def destroy
-    @membership.delete! unless @membership.nil? && curren_user.board_admin
+    @membership = @board.memberships.where(user_id: params[:user_id]).first
+    @membership.update_attribute('admin', true)
   end
 
   private
 
-  def current_board
-    @board = Board.find_by_id(params[:id])
+  def load_board
+    @board = Board.find(params[:board_id])
+  end
+
+  def check_user_permission
+    if current_user.memberships.exists?(board_id: @board.id, admin: true)
+      flash[:danger] = 'You are not admin for this board'
+      redirect_back fallback_location: root_path
+    end
+  end
+
+  def board_admin(board)
+    administrated_boards.exists?(id: board.id)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,4 @@ class User < ApplicationRecord
   has_many :memberships
   has_many :boards, through: :memberships
   has_many :administrated_boards, -> { where(admin: true) }, class_name: 'Board', through: :membership
-
-  def board_admin(board)
-    administrated_boards.exists?(id: board.id)
-  end
 end
