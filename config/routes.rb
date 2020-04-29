27f55631f780c17e8aacbf9db649553c2a4bfb88
psidae <akson.rill@gmail.com>
2020-04-29 14:06:03 +0300

    Create members routes, refactor app.css & create board controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' } 
+  resources :boards do
+    resources :memberships, only: %i[create]
+    put 'members/:user_id/admin' => 'memberships#admin'
+  end
+
+  devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' }
 end
