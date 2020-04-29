c8833cd89b1804d7759c5610b4013f347a6293ef
Tanya Gurova <tgurova@star.global>
2020-04-29 23:14:26 +0300

    Error handling & check board admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,10 @@ class User < ApplicationRecord
 
   has_many :memberships, dependent: :destroy
   has_many :boards, through: :memberships
-  has_many :administrated_boards, -> { where(admin: true) }, class_name: 'Board', through: :membership
+  validates :email, uniqueness: true
+
+  has_many :administrated_boards, -> { where(memberships: {admin: true}) }, class_name: 'Board', 
+  through: :memberships, source: :board
 
   scope :search, ->(email) { where('email LIKE ?', "%#{email}%") }
 end
