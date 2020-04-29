a7b00ba061182264968c34295ec75de61ecf2a7f
psidae <akson.rill@gmail.com>
2020-04-29 15:41:16 +0300

    Create user controller & add search scope, update board view

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,6 @@ class User < ApplicationRecord
   has_many :memberships, dependent: :destroy
   has_many :boards, through: :memberships
   has_many :administrated_boards, -> { where(admin: true) }, class_name: 'Board', through: :membership
+
+  scope :search, ->(email) { where('email LIKE ?', "%#{email}%") }
 end
