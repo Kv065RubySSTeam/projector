3f424e7c0afa935dd32bc264122199c1c1b4173a
kerono4ka <olgha.tovstenko@gmail.com>
2020-04-29 21:41:14 +0300

    merge with header, sidebar, footer

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,16 +17,18 @@ require('jquery')
 // const imagePath = (name) => images(name, true)
 
 // BEGIN Vendor JS
-    require('frest/app-assets/vendors/js/vendors.min.js')
-    require('frest/app-assets/fonts/LivIconsEvo/js/LivIconsEvo.tools.js')
-    require('frest/app-assets/fonts/LivIconsEvo/js/LivIconsEvo.defaults.js')
-    require('frest/app-assets/fonts/LivIconsEvo/js/LivIconsEvo.min.js')
+    require('frest/app-assets/vendors/js/vendors.min.js');
+    // require('frest/app-assets/fonts/LivIconsEvo/js/LivIconsEvo.tools.js')
+    // require('frest/app-assets/fonts/LivIconsEvo/js/LivIconsEvo.defaults.js')
+    // require('frest/app-assets/fonts/LivIconsEvo/js/LivIconsEvo.min.js')
 
 // BEGIN: Theme JS
-	  require('frest/app-assets/js/scripts/configs/horizontal-menu.js')
-    require('frest/app-assets/js/core/app-menu.js')
-    require('frest/app-assets/js/core/app.js')
-    require('frest/app-assets/js/scripts/components.js')
-    require('frest/app-assets/js/scripts/footer.js')
 
+    require('frest/app-assets/js/core/app.js')
+	  require('frest/app-assets/js/scripts/configs/horizontal-menu.js')
+    require('frest/app-assets/js/core/app-menu.min.js')
+    require('frest/app-assets/js/scripts/components.js')
+
+    require('frest/app-assets/js/scripts/footer.js')
     require('javascript/packs/error_fields.js')
+
