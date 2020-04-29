930d0405e7b7d5ee769a83ff1f4244f19f3edeb1
Tanya Gurova <tgurova@star.global>
2020-04-29 14:09:16 +0300

    scheme update

--- db/schema.rb
+++ db/schema.rb
@@ -42,6 +42,10 @@ ActiveRecord::Schema.define(version: 2020_04_29_084421) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
