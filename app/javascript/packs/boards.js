2978d697a514eb69d625baaa78beea47ad31711e
Iryna Volyk <ira.volyk13@gmail.com>
2020-04-30 22:12:53 +0300

    Add new styles and js for boards

---
+++ app/javascript/packs/boards.js
@@ -0,0 +1,10 @@
+//For boards
+require('frest/app-assets/vendors/js/ui/jquery.sticky.js')
+require('frest/app-assets/vendors/js/editors/quill/quill.min.js')
+require('frest/app-assets/vendors/js/pickers/pickadate/picker.js')
+require('frest/app-assets/vendors/js/pickers/pickadate/picker.date.js')
+require('frest/app-assets/vendors/js/pickers/daterange/moment.min.js')
+require('frest/app-assets/vendors/js/pickers/daterange/daterangepicker.js')
+require('frest/app-assets/vendors/js/forms/select/select2.full.min.js')
+require('frest/app-assets/vendors/js/extensions/dragula.min.js')
+require('frest/app-assets/js/scripts/pages/app-todo.js')
