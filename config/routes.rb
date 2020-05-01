93f3c3f14c28a2dbf1b78fa1c6184b79f5f7a139
etherell <kostyaetherell@gmail.com>
2020-05-01 11:05:30 +0300

    Style changes in show view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "boards#index"
   resources :boards
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' } 
   resources :users, only: [:show]
