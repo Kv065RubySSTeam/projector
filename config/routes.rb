438e04e4a434b3c668f846577cb5153efe6474d4
Tanya Gurova <tgurova@star.global>
2020-05-01 21:29:03 +0300

    adding user to the board

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :boards
   resources :users, only: [:show]
   get 'users/index'
-  post '/boards/:id/memberships/' => 'memberships#create', as: :memberships_create
+  get '/boards/:id/memberships/' => 'memberships#create', as: :memberships_create
 
   resources :boards do
     # resources :memberships, only: %i[create]
