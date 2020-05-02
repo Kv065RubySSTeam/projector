6e91d279a644a1c98b9673a5b669ddbb0bbc09b4
etherell <kostyaetherell@gmail.com>
2020-05-02 17:44:03 +0300

    Add reconfirmable to user

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :boards
+  root to: "boards#index"
   devise_for :users, path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'signup' } 
   resources :users, only: [:show]
 end
