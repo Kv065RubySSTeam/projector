40d24a6edd6a1765396e8e706e4174b7d8a69918
Synkevych Roman <sunkevu4@gmail.com>
2020-05-04 19:47:34 +0300

    Add ajax - update and delete functionality

---
+++ app/javascript/packs/update_column.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+  var allColumns = $('.kanban-title-board').length;
+
+  for (let i = 0; i < allColumns; i++) {
+    let col = $('.kanban-title-board')[i];
+
+     // find column id, columnTitle, board_id
+    col.addEventListener('blur', function(event) {
+      var columnTitle = event.srcElement.textContent.trim();
+      var board_id = Number(
+         event.srcElement.closest('.card').getAttribute('data-board-id')
+       );
+       var column_id = Number(
+         event.srcElement.closest('.kanban-board').getAttribute('data-order')
+       );
+       console.log(board_id, column_id);
+        updateColumn(board_id, column_id, columnTitle);
+        col.closest('.kanban-board').remove();
+    });
+
+    function updateColumn(board_id, column_id, columnTitle) {
+      // update column at the database
+      $.ajax({
+        url: board_id + '/columns/' + column_id,
+        method: 'PATCH',
+        headers: {
+          'X-CSRF-Token': document.getElementsByName('csrf-token')[0].content
+        },
+        data: {
+          'column[name]': columnTitle
+        }
+      });
+    }
+   }
+});
