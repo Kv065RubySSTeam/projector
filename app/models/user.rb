5108e04438b3b9d0b2329dde19c3885202c69efd
Olha Tovstenko <37984886+Kerono4ka@users.noreply.github.com>
2020-05-06 13:17:19 +0300

    update column dependency

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :boards, dependent: :nullify
+  has_many :columns
   devise :database_authenticatable,
          :registerable, :validatable,
          :confirmable, :recoverable
