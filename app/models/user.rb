282432549bb5aa40185285faeca56b473d7c4032
Tanya <saputo1990@gmail.com>
2020-05-07 16:39:43 +0300

    Add facebook omniauth, implement facebook auth logic, update login view with facebook button

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,23 @@ class User < ApplicationRecord
   has_many :boards, dependent: :nullify
   devise :database_authenticatable,
          :registerable, :validatable,
-         :confirmable, :recoverable
+         :confirmable, :recoverable,
+         :omniauthable, omniauth_providers: %i[facebook]
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+        user.email = data['email'] if user.email.blank?
+      end
+    end
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+    end
+  end
 end
