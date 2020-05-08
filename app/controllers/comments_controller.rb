08c852e577bb533232e5d585c0e7d6cdd0406cb5
etherell <kostyaetherell@gmail.com>
2020-05-08 15:21:54 +0300

    Add comment edit, comments controller refactor, fixing styles in comment view, convert erb to haml

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,41 @@
 class CommentsController < ApplicationController
   before_action :find_column!
+  before_action :find_comment!, only: [:edit, :update, :destroy]
   before_action :find_card!, except: [:new]
 
   def new
-    @card = Card.find(params[:id])
+    @card = Card.find(params[:id])  # Only here card id in params
   end
   
   def create
     @comment = @card.comments.build(comment_params)
     @comment.user = current_user
-    @comment.save
+    
+    respond_to do |f|   
+      if @comment.save
+        f.js { flash[:success] = "Comment was successfully created." }
+      else
+        f.js { flash[:error] = "With creatind comment were an error!" }
+      end
+    end
   end
 
-  def edit
+  def edit; end
+
+  def update
+    @comment.update(comment_params)
+    @comments = @card.comments.order(created_at: :desc)
   end
   
   def destroy
-    @comment = @card.comments.find(params[:id])
     @comment.destroy
   end
 
   private
+
+  def find_comment!
+    @comment = Comment.find(params[:id])
+  end
 
   def find_column!
     @column = Column.find(params[:column_id])
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :card
+  validates :name, length: { within: 2..255 }
 end
