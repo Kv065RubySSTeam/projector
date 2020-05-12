3aabf4058d6eda9e392195bce0e5197bdd26e09c
Synkevych Roman <sunkevu4@gmail.com>
2020-05-12 14:19:47 +0300

    Add respond_to to card controller

--- app/models/column.rb
+++ app/models/column.rb
@@ -3,6 +3,12 @@ class Column < ApplicationRecord
 
   belongs_to :board
   belongs_to :user
+  has_many :cards, dependent: :destroy
   validates :name, length: { within: 2..50 }
   validates :position, uniqueness: { scope: :board }
+  
+  def last_card_position
+    self.cards.maximum(:position)
+  end
+  
 end
