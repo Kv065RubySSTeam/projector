d30248ba149f89009cc4b23bccf9e9d26a70d308
psidae <akson.rill@gmail.com>
2020-06-07 21:04:13 +0300

    Create documentation for user(Model, Controller, Helper)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,5 +1,7 @@
 module UsersHelper
-  
+  # Check if user has an avatar and return it
+  # @param user
+  # @return [String] *.jpg
   def get_avatar(user)
     if user.avatar.attached?
       user.avatar
@@ -8,6 +10,9 @@ module UsersHelper
     end
   end
 
+  # Return user full name
+  # @param user
+  # @return [String] full name
   def get_full_user_name(user)
     [user.first_name, user.last_name].join(" ")
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  # @!group Associations
   has_many :columns, dependent: :destroy
   has_many :cards, dependent: :destroy
   has_many :assigned_card, class_name: 'Card'
@@ -7,7 +8,9 @@ class User < ApplicationRecord
   has_many :boards, through: :memberships
   has_many :likes, dependent: :destroy
   has_one_attached :avatar
+  # @!endgroup
 
+  # @!group Validations
   validates :email, uniqueness: true
   validates :email, uniqueness: true
   validates :first_name, length: { within: 1..100 }
@@ -15,7 +18,10 @@ class User < ApplicationRecord
   validates :avatar, content_type:
     { in: ['image/png', 'image/jpg', 'image/jpeg'],
       message: "format is wrong, please use JPG, PNG or JPEG" }
+  # @!endgroup
 
+  # Include default devise modules. Others available are:
+  # :confirmable, :omniauthable, :recoverable
   devise :database_authenticatable,
          :registerable, :recoverable, :validatable,
          :async, :confirmable,
@@ -24,10 +30,12 @@ class User < ApplicationRecord
   attribute :remove_avatar, :boolean,  default: false
   after_save :purge_avatar, if: :remove_avatar
 
+  # @see https://www.rubydoc.info/github/plataformatec/devise/Devise%2FModels%2FAuthenticatable:send_devise_notification
   def send_devise_notification(notification, *args)
     devise_mailer.send(notification, self, *args).deliver_later
   end
 
+  # @see https://rubydoc.info/github/cofiem/aptness/master/User#new_with_session-class_method
   def self.new_with_session(params, session)
     super.tap do |user|
       if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
@@ -36,6 +44,7 @@ class User < ApplicationRecord
     end
   end
 
+  # @see https://rubydoc.info/github/cofiem/aptness/master/User#from_omniauth-class_method
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
@@ -45,24 +54,38 @@ class User < ApplicationRecord
     end
   end
 
+  # Return user full name
+  # @return [String]
   def full_name
     "#{first_name} #{last_name}"
   end
 
+  # @!method administrated_boards
+  #   Administrated boards checks if user is admin on current board
+  # @return [Membership] where admin: true
   has_many :administrated_boards, -> { where(memberships: { admin: true }) }, class_name: 'Board',
                                                                               through: :memberships,
                                                                               source: :board
 
+  # @!method
+  #   Search user by email, first_name and last_name
+  #   @return [User]
   scope :search, lambda { |user|
     where("concat_ws('OR', LOWER(email), LOWER(first_name), LOWER(last_name)) LIKE LOWER(?)", "%#{user}%")
   }
 
   private
+  # Directly purges the attachment (i.e. destroys the blob and attachment and deletes the file on the service).
+  # @return [Avatar]
   def purge_avatar
+    # Purges the attachment through the queuing system.
     avatar.purge_later
   end
-  
+
   protected
+  # Callback to overwrite if confirmation is required or not.
+  # @see https://www.rubydoc.info/github/plataformatec/devise/Devise%2FModels%2FConfirmable:confirmation_required%3F
+  # @return [Boolean]
   def confirmation_required?
     false
   end
