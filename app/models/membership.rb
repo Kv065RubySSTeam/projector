6e9b03930fa53dd3a8d5c4045f268302d76c00dd
Roman <sunkevu4@gmail.com>
2020-06-19 19:56:03 +0300

    Add doc for memberships controller and model. (#97)

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,13 +1,22 @@
+# The membership belongs to the board,
+# the board is present and can have any amount of users.
+# The membership has uniqueness of user on board scope, and need to be present.
 class Membership < ApplicationRecord
+  # @!group Associations
   belongs_to :board
   belongs_to :user
+  # @!endgroup
 
+  # @!group Validations
   validates_uniqueness_of :user_id, scope: :board_id, presence: true
+  # @!endgroup
 
+  # @return [Boolean] with TRUE value
   def admin!
     update(admin: true)
   end
 
+  # @return [Boolean] with FALSE value 
   def remove_admin!
     update(admin: false)
   end
